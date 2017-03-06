9fd671ff61c147178a9107c7ca55a7cc4af685d6
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-03-06 21:32:24 +0530

    App uploaded

---
+++ UdacityBusinessCard/app/src/androidTest/java/com/androidexample/udacitybusinesscard/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.androidexample.udacitybusinesscard;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
